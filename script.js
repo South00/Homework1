48712b70937a9a76a49a8e188da2646617728c44
South00 <mr.dima9@gmail.com>
2016-05-07 17:17:12 +0400

    Правки в js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 function printmail() {
     alert("mail: "+lbMail.value+"\npassword: "+lbPassword.value);
 }
+
+Слишком много дейстивй в одной строчке кода, разбей на нексолько строк - улучшится читаемость
